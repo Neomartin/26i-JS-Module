7daa9f24c05bd219464213b865fdf4b24c569b58
Martín Rodríguez <neomartinr@gmail.com>
2022-03-08 20:12:16 -0300

    Filter array method

--- js/forEach.js
+++ js/forEach.js
@@ -1,4 +1,4 @@
-const paisesMundialObject = [
+const paisesMundialObjects = [
     {
         name: 'Francia',
         year: 2018
@@ -83,4 +83,105 @@ const paisesMundialObject = [
         name: 'Uruguay',
         year: 1930
     },
-]
+    {
+        name: 'Inglaterra',
+        year: 1966
+    },
+    {
+        name: 'Brasil',
+        year: 1962
+    },
+    {
+        name: 'Brasil',
+        year: 1958
+    },
+    {
+        name: 'Alemania',
+        year: 1954
+    },
+    {
+        name: 'Uruguay',
+        year: 1950
+    },
+    {
+        name: 'Italia',
+        year: 1938
+    },
+    {
+        name: 'Italia',
+        year: 1934
+    },
+    {
+        name: 'Uruguay',
+        year: 1930
+    },
+];
+console.log('ANTES DE EJECUTAR MODIFICACIONEs',paisesMundialObjects) 
+
+// let paisesMundialBRASIL = paisesMundialObjects.map((pais)=> {
+//     return pais;
+// })
+// const brasil = [];
+const resto = [];
+const restoFilterMethod = paisesMundialObjects.filter(seleccion =>{ 
+    if(seleccion.name !== 'Brasil') {
+        return true 
+    }
+    return false
+});
+
+console.log(restoFilterMethod)
+// paisesMundialObjects.forEach(paisGanador => {
+//     if(paisGanador.name !== 'Brasil') {
+//         resto.push(paisGanador)
+//     } 
+// })
+console.log(resto)
+
+
+
+// paisesMundialObjects.forEach((pais)=> {
+
+//     if(pais.name === 'Brasil') {
+//         paisesMundialBRASIL.push(pais)
+//     }
+
+// })
+
+// console.log('paisesMundialBRASIL',paisesMundialBRASIL)
+// console.log('Array original',paisesMundialObjects)
+
+
+
+
+
+
+
+
+
+
+
+// console.log(paisesMundialObjects)
+// let brasilCampeonatosGanados = [];
+// paisesMundialObjects.forEach((paisGanador, indice) => {
+//         // console.log(paisGanador)
+//         //Preguntar si el país que estamos analizando es Brasil
+//         //Si el país fue Brasil vamos a insertar el año dentro del array brasilCampeonatos
+//         if(paisGanador.name === 'Brasil') {
+//             brasilCampeonatosGanados.push(paisGanador.year)
+//             console.log(paisGanador.name, indice)
+//             paisesMundialObjects.splice(indice, 1)
+//         }
+//         document.write(`<div class="card">El país que ganó el mundial del año 
+//                             <span> ${paisGanador.year} </span> fue ${paisGanador.name}</div>`)
+// });
+
+// let paisesMayuscula = paisesMundialObjects.map(element => {
+//     // console.log(paisGanador)
+//     return element.name.toUpperCase();
+// })
+
+// console.log(paisesMundialObjects)
+// console.log(paisesMayuscula)
+
+
